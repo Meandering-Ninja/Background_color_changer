51ee56eaadd0ba38649e9e797d8c3329714f0f67
Meandering-Ninja <161959791+Meandering-Ninja@users.noreply.github.com>
2024-07-09 16:19:50 +0530

    Adding comment in js file

--- Background color changer/Background color changer.js
+++ Background color changer/Background color changer.js
@@ -3,9 +3,9 @@ let mode ='light';
 a.onclick = () =>{
     if(mode == 'light'){
         mode='dark';
-        document.querySelector("body").style.backgroundColor = 'black';
+        document.querySelector("body").style.backgroundColor = 'black';            //You can change color as per your choice
         document.querySelector("h1").style.backgroundColor = 'white';
-        alert("Changing background to Black")
+        alert("Changing background to Black")                                        //Remove Alert message if not required.
         }
         else{
             mode='light';
